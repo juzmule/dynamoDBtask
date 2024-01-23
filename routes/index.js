ece8ad0774ac52ee1c0aac87db2b870d5c04c9f8
juzmule <stian.lundberg96@gmail.com>
2024-01-23 12:11:52 +0100

    DynamoDB update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
+var CyclicDB = require('@cyclic.sh/dynamodb');
+var db = CyclicDB(process.env.CYCLIC_DB);
+var Content = db.collection('content');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  let list = await Content.list();
+  res.send(list);
+});
+
+router.post('/', async function(req, res, next) {
+  const { content } = req.body;
+  await Content.set(content)
+  res.end();
 });
 
 module.exports = router;
